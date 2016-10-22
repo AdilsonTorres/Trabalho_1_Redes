3e2ff41abaf04e5e6902366fce04a44ea1661028
Adilson Torres <adilson.torres@outlook.com>
2016-10-22 11:04:09 -0200

    Create a eclipse project and add Conexao and Servidor Template

---
+++ src/usp/ServidorWeb.java
@@ -0,0 +1,26 @@
+package usp;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class ServidorWeb {
+  public static void main(String[] args) {
+    int porta; 		//porta aonde o servidor vai aguardar por pedido de conexao.
+    ServerSocket sw; 	//Socket servidor
+	 
+	//de um valor para a porta!
+    
+    try {
+
+      //crie aqui o socket Servidor!!!!!!!!(objeto = new ServerSocket(parametros))
+
+      while(true) {
+
+	  }
+    }
+    catch(IOException e) {
+      System.err.println("Servidor foi abortardo");
+    }  
+  }
+}
