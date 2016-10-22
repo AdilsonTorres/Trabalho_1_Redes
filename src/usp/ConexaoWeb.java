ab329b19cf71aad128a0111d245edcd84d585ca2
Jhonata Antunes <jhonata.antunes@controlid.com.br>
2016-10-22 18:53:03 -0200

    Implementação até o passo 4

--- src/usp/ConexaoWeb.java
+++ src/usp/ConexaoWeb.java
@@ -4,37 +4,48 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class ConexaoWeb {
-  Socket socket; 					//socket que vai tratar com o cliente.
-  static String arqi = "index.html"; 	//se nao for passado um arquivo, o servidor fornecera a pagina index.html
+public class ConexaoWeb
+{
+	Socket socket; 					//socket que vai tratar com o cliente.
+	static String arqi = "index.html"; 	//se nao for passado um arquivo, o servidor fornecera a pagina index.html
+	
+	//coloque aqui o construtor
+	public ConexaoWeb(Socket socket)
+	{
+		this.socket = socket;
+	}
+	
+	//metodo TrataConexao, aqui serao trocadas informacoes com o Browser...
 
-  //coloque aqui o construtor
-  
-
-  //metodo TrataConexao, aqui serao trocadas informacoes com o Browser...
-
-  public void TrataConexao() {
-    String metodo=""; 				//String que vai guardar o metodo HTTP requerido
-    String ct; 					//String que guarda o tipo de arquivo: text/html;image/gif....
-    String versao = ""; 			//String que guarda a versao do Protocolo.
-    File arquivo; 				//Objeto para os arquivos que vao ser enviados. 
-    String NomeArq; 				//String para o nome do arquivo.
-    String raiz = "."; 				//String para o diretorio raiz.
-	String inicio;				//String para guardar o inicio da linha
-	String senha_user="";		//String para armazenar o nome e a senha do usuario
-	Date now = new Date();
+	public void TrataConexao() 
+	{
+	    String metodo=""; 		//String que vai guardar o metodo HTTP requerido
+	    String ct; 				//String que guarda o tipo de arquivo: text/html;image/gif....
+	    String versao = ""; 	//String que guarda a versao do Protocolo.
+	    File arquivo; 			//Objeto para os arquivos que vao ser enviados. 
+	    String NomeArq; 		//String para o nome do arquivo.
+	    String raiz = "."; 		//String para o diretorio raiz.
+		String inicio;			//String para guardar o inicio da linha
+		String senha_user = "";	//String para armazenar o nome e a senha do usuario
+		Date now = new Date();
 		
-    try {
-
-		//Coloque aqui o acesso aos streams do socket!
-		
-		//Coloque aqui o procedimento para ler toda a mensagem do cliente. Imprima na tela com System.out.println()!
-		
+		try 
+		{
+			//Coloque aqui o acesso aos streams do socket!
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
+			
+			//Coloque aqui o procedimento para ler toda a mensagem do cliente. Imprima na tela com System.out.println()!
+			String linha;
+			while (!(linha = entrada.readLine()).equals(""))
+			{
+				System.out.println(linha);
+			}
+			
 			/* Para a segunda parte, ignore para a primeira!
   			// Enviar o arquivo
-			try {
-				
-			
+			try 
+			{
 			
 			//Crie aqui o objeto do tipo File
 			
@@ -50,38 +61,29 @@ public class ConexaoWeb {
 			}   
 			*/
 		
-	}
-	catch(IOException e)
-	{
-	}
-	
-    //Fecha o socket.
-    try {
-      socket.close();
-    }
-    catch(IOException e) {
-    }
-  }
-
-//Funcao que retorna o tipo do arquivo.
-
-  public String TipoArquivo(String nome) {
-    if(nome.endsWith(".html") || nome.endsWith(".htm")) return "text/html";
-    else if(nome.endsWith(".txt") || nome.endsWith(".java")) return "text/plain";
-    else if(nome.endsWith(".gif")) return "image/gif";
-    else if(nome.endsWith(".class"))  return "application/octed-stream";
-    else if( nome.endsWith(".jpg") || nome.endsWith(".jpeg") ) return "image/jpeg";
-    else return "text/plain";
-  }
-}
+		}
+		catch(IOException e)
+		{
+		}
 		
+	    //Fecha o socket.
+	    try 
+	    {
+	    	socket.close();
+	    }
+	    catch(IOException e) 
+	    {
+	    }
+	}
 
-
-
-
-
-
-
-
-
-
+	//Funcao que retorna o tipo do arquivo.
+	public String TipoArquivo(String nome) 
+	{
+		if (nome.endsWith(".html") || nome.endsWith(".htm")) return "text/html";
+		else if (nome.endsWith(".txt") || nome.endsWith(".java")) return "text/plain";
+		else if (nome.endsWith(".gif")) return "image/gif";
+		else if (nome.endsWith(".class"))  return "application/octed-stream";
+		else if ( nome.endsWith(".jpg") || nome.endsWith(".jpeg") ) return "image/jpeg";
+		else return "text/plain";
+	}
+}
--- src/usp/ServidorWeb.java
+++ src/usp/ServidorWeb.java
@@ -4,23 +4,30 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class ServidorWeb {
-  public static void main(String[] args) {
-    int porta; 		//porta aonde o servidor vai aguardar por pedido de conexao.
-    ServerSocket sw; 	//Socket servidor
-	 
-	//de um valor para a porta!
-    
-    try {
-
-      //crie aqui o socket Servidor!!!!!!!!(objeto = new ServerSocket(parametros))
-
-      while(true) {
-
-	  }
-    }
-    catch(IOException e) {
-      System.err.println("Servidor foi abortardo");
-    }  
-  }
+public class ServidorWeb 
+{
+	public static void main(String[] args) 
+	{
+		int porta = 9090; //porta aonde o servidor vai aguardar por pedido de conexao.
+		ServerSocket sw;  //Socket servidor
+		ConexaoWeb conexao;
+		
+		try 
+		{	
+			//crie aqui o socket Servidor!!!!!!!!(objeto = new ServerSocket(parametros))
+			sw = new ServerSocket(porta);
+			conexao = new ConexaoWeb(sw.accept());
+			conexao.TrataConexao();
+			
+			while(true) 
+			{
+		
+			}
+		}
+		catch(IOException e) 
+		{
+			System.err.println("Servidor foi abortardo");
+			System.err.println(e.getMessage());
+		}  
+	}
 }
