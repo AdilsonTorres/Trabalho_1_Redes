d51f6eaf0153278a76d170d2761cdd5881d813e0
Adilson Torres <adilson.torres@outlook.com>
2016-10-24 12:05:29 -0200

    Filtrado arquivo de log, inclusão  de autenticação para a pasta /secret

--- src/usp/ConexaoWeb.java
+++ src/usp/ConexaoWeb.java
@@ -8,7 +8,7 @@ public class ConexaoWeb extends Thread
 {
 	Socket socket; 					    //socket que vai tratar com o cliente.
 	static String arqi = "index.html"; 	//se nao for passado um arquivo, o servidor fornecera a pagina index.html
-	DataOutputStream log;               //objeto para aruivo de log
+	DataOutputStream log;               //objeto para arquivo de log
 	
 	//coloque aqui o construtor
 	public ConexaoWeb(Socket socket)
@@ -28,19 +28,21 @@ public class ConexaoWeb extends Thread
 
 	public void run() 
 	{
-	    String metodo=""; 		   //String que vai guardar o metodo HTTP requerido
-	    String ct; 				   //String que guarda o tipo de arquivo: text/html;image/gif....
-	    String versao = ""; 	   //String que guarda a versao do Protocolo.
-	    File arquivo; 		 	   //Objeto para os arquivos que vao ser enviados. 
-	    String NomeArq; 		   //String para o nome do arquivo.
-	    String raiz = "root"; 	   //String para o diretorio raiz.
-	    String host = "host0";     //String para host padrao.
-	    String datapath = "";      //String para o caminho final do arquivo.
-		String inicio;			   //String para guardar o inicio da linha
-		String senha_user = "";	   //String para armazenar o nome e a senha do usuario
-		String primeiraLinha = ""; //String para armazenar a primeira linha do cabeçalho 
-		String linha;              //String para leitura de cabeçalho
-		String ip;                 //String para id do cliente
+	    String metodo=""; 		   			//String que vai guardar o metodo HTTP requerido
+	    String ct; 				   			//String que guarda o tipo de arquivo: text/html;image/gif....
+	    String versao = ""; 	   			//String que guarda a versao do Protocolo.
+	    File arquivo; 		 	   			//Objeto para os arquivos que vao ser enviados. 
+	    String NomeArq; 		   			//String para o nome do arquivo.
+	    String raiz = "root"; 	   			//String para o diretorio raiz.
+	    String host = "host0";     			//String para host padrao.
+	    String datapath = "";      			//String para o caminho final do arquivo.
+		String inicio;			   			//String para guardar o inicio da linha
+		//String senha_user = "";	  			//String para armazenar o nome e a senha do usuario
+		String primeiraLinha = ""; 			//String para armazenar a primeira linha do cabeçalho 
+		String linha;                      	//String para leitura de cabeçalho
+		String ip;                 			//String para id do cliente
+		String tipo_codificacao = "";
+		String senha_user_codificado = ""; 	//String para armazenar o nome e a senha codificado
 		Date now = new Date();
 		
 		try 
@@ -73,23 +75,41 @@ public class ConexaoWeb extends Thread
 				metodo = st.nextToken();
 				NomeArq = st.nextToken();
 				versao = st.nextToken();
+				
+				// LOG
+				log.writeBytes(ip + " - [" + now.toString() + "]\n");
+				
 				// Jogar para dentro do GET *****************
 				if (NomeArq.endsWith("/")) NomeArq = NomeArq + arqi;
 				ct = TipoArquivo(NomeArq);
 				
-				// LOG
-				log.writeBytes(ip + " - [" + now.toString() + "]\n");
+				
 				
 				// Lê todo o cabeçalho
 				while (!(linha = entrada.readLine()).equals(""))
 				{
 					st = new StringTokenizer(linha);
-					
-					if (st.nextToken().startsWith("Host"))
+					String next = st.nextToken();
+					System.out.println(linha);
+					if (next.startsWith("Host"))
 					{
-						host = st.nextToken();
+						String host_received = st.nextToken();
+						// Por padrão o Host é localhost:9090, mantendo assim o host padrão.
+						if (host_received.startsWith("localhost"))
+						{
+							
+						}else{
+							host = host_received;
+						}
+						
 						System.out.println("Host: " + host);
 						System.out.println("Nome do arquivo: " + NomeArq);
+					}
+					if (next.startsWith("Authorization"))
+					{
+						tipo_codificacao = st.nextToken();
+						senha_user_codificado = st.nextToken();
+						
 					}
 				}
 				
@@ -98,6 +118,25 @@ public class ConexaoWeb extends Thread
 					System.out.println("Lendo arquivo");
 					datapath = host + NomeArq;
 					System.out.println("Datapath: " + datapath);
+					
+					//Área de acesso exclusivo com autenticação.
+					if(datapath.startsWith("host0/secret/"))
+					{
+						// Caso não senha o usuário: alguem com a senha: senha não entra.
+						// Assim como não tenha requisitado com Authorization.
+						if(senha_user_codificado.equals("YWxndWVtOnNlbmhh"))
+						{
+							
+						}else
+						{
+							saida.writeBytes("HTTP/1.1 401 Unauthorized\n");
+							saida.writeBytes("Content-Type: text/html\n");
+							saida.writeBytes("WWW-Authenticate: Basic realm=”System Administrator”");
+							saida.writeBytes("\n");
+							saida.close();							
+						}
+					}
+					
 					//Crie aqui o objeto do tipo File
 					arquivo = new File(raiz,datapath.substring(0,datapath.length()));
 					
